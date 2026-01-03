4e1c406c5427689744db0b1df72b4ceabc496836
abir_zennir <abirzennir3@gmail.com>
2026-01-03 21:17:03 +0100

    add

--- service-voiture/src/main/java/com/example/voiture/VoitureApplication.java
+++ service-voiture/src/main/java/com/example/voiture/VoitureApplication.java
@@ -1,11 +1,11 @@
-package com.example.voiture;
+package com.tp.voiture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VoitureApplication {
+public class ServiceVoitureApplication {
     public static void main(String[] args) {
-        SpringApplication.run(VoitureApplication.class, args);
+        SpringApplication.run(ServiceVoitureApplication.class, args);
     }
 }
